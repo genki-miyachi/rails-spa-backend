e331c3865ed02a1deb66c68bbf4d5b661dd14d7c
Genki Miyachi <genki.miyachi@techouse.jp>
2021-05-13 15:17:28 +0900

    commit

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+
+users = User.create([
+                      { name: '宮地元気', furigana: 'みやちげんき', age: 27 },
+                      { name: '篠田実桜', furigana: 'しのだみお', age: 26 }
+                    ])
